6b86e92d77130a9f683941901c8fd27a51af9a94
Brian Esquivel <brian.esquive@gmail.com>
2024-01-26 02:34:16 -0600

    Ejercicio 3

---
+++ Exercises/exercise3.cs
@@ -0,0 +1,20 @@
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+ //AQUÍ EMPIEZA =>
+ 
+//  long num1 = 0;
+//  long num2 = 1;
+//  long Fibonacci = 0;
+ 
+//  for(int i = 0; i <= 50; i++){
+//      Console.WriteLine(Fibonacci);
+//      Fibonacci = num1 + num2;
+//      num1 = num2;
+//      num2 = Fibonacci;
+//  }
