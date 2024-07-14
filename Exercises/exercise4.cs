d5b3a1c285fdc554b9b9da5c1073e0b0e0e2c10f
Brian Esquivel <brian.esquive@gmail.com>
2024-07-13 21:05:23 -0600

    Exercise 4

---
+++ Exercises/exercise4.cs
@@ -0,0 +1,27 @@
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
+
+ //AQUI EMPIEZA =>
+
+//  var i = 0;
+
+//  while(i<=100){
+//         if(isPrime(i)){
+//             Console.WriteLine($"{i}\n");
+//         }
+//         i++;
+//  }
+
+// static bool isPrime(int n){
+//         if(n<=1){
+//             return false;
+//         }
+//         for(int i = 2; i<=Math.Sqrt(n); i++){
+//             if(n%i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+// }
